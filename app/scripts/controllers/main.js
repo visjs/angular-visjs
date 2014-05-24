f7b9236710e26b3383dbdec4ec0271202f6fe9f9
Cengizism <culusoy@ask-cs.com>
2014-05-24 19:08:10 +0200

    initial commit - angular project exported with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngVisjsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
