bb4783f7c664fad826bba8fb05c01bd69ee557ec
Cengizism <culusoy@ask-cs.com>
2014-07-11 09:39:47 +0200

    work on progress - broken

--- app/scripts/ng-vis.js
+++ app/scripts/ng-vis.js
@@ -1,29 +1,45 @@
 'use strict';
 
+// 1. Make filters from range indicators
+// 2. Implement a controller for directive
+// 3. Take data processing functions into the controller
+// 4. Implement $destroy event catchers for directive element and scope
+// 5. Supply callbacks via their own functions
+// 6. Give an object back which holds information about timeline itself
+
 angular.module('ngVis', []).
+
 
   constant(
   'options', {
+
     orientation: 'bottom',
-    direction: 'horizontal',
+    direction: 'horizontal', // 'horizontal' or 'vertical'
     autoResize: true,
     stack: true,
+
     editable: {
       updateTime: false,
       updateGroup: false,
       add: false,
       remove: false
     },
+
     selectable: true,
-    snap: null,
+    snap: null, // will be specified after timeaxis is created
+
     min: null,
     max: null,
-    zoomMin: 10,
-    zoomMax: 1000 * 60 * 60 * 24 * 365 * 10000,
+    zoomMin: 10,                                // milliseconds
+    zoomMax: 1000 * 60 * 60 * 24 * 365 * 10000, // milliseconds
+    // moveable: true, // TODO: option movable
+    // zoomable: true, // TODO: option zoom-able
+
     showMinorLabels: true,
     showMajorLabels: true,
     showCurrentTime: false,
     showCustomTime: false,
+
     type: 'box',
     align: 'center',
     margin: {
@@ -31,16 +47,44 @@ angular.module('ngVis', []).
       item: 10
     },
     padding: 5,
+
+
     debug: false,
+    // align: 'center',
+    // autoResize: true,
+    // editable: true,
     start: null,
     end: null,
     height: null,
     width: '100%',
+    //    margin: {
+    //      axis: 20,
+    //      item: 10
+    //    },
+    //    min: null,
+    //    max: null,
     maxHeight: null,
+    //    orientation: 'bottom',
+    //    padding: 5,
+    //    selectable: true,
+    //    showCurrentTime: true,
+    //    showCustomTime: true,
+    //    showMajorLabels: true,
+    //    showMinorLabels: true,
+    //    type: 'box', // dot | point
+    //    zoomMin: 1000,
+    //    zoomMax: 1000 * 60 * 60 * 24 * 30 * 12 * 10,
     groupOrder: 'content'
   }).
 
-  factory('Moment', function () { return vis.moment() }).
+
+  factory(
+  'Moment',
+  function ()
+  {
+    return vis.moment()
+  }).
+
 
   factory(
   'process',
@@ -149,6 +193,8 @@ angular.module('ngVis', []).
           data: '=',
           options: '=',
           // timeline: '='
+          add: '&',
+          edit: '&'
         },
 
         controller: function (Moment)
@@ -204,6 +250,13 @@ angular.module('ngVis', []).
             {
               // console.log('options changed!');
 
+              if (! angular.isDefined(_options))
+              {
+                _options = {
+                  defaults: true
+                }
+              }
+
               if (_options.defaults)
               {
                 _timeline.clear({options: true});
@@ -222,7 +275,261 @@ angular.module('ngVis', []).
               }
             }
           );
+
+          angular.extend(
+            scope.timeline,
+            {
+              customDate: _timeline.getCustomTime(),
+
+              setOptions: function (options) { _timeline.setOptions(options) },
+
+              getSelection: function () { return _timeline.getSelection() },
+
+              setSelection: function (selection) { return _timeline.setSelection(selection) },
+
+              getWindow: function () { return _timeline.getWindow() },
+
+              setWindow: function (start, end) { return _timeline.setWindow(start, end) },
+
+              getCustomTime: function () { return _timeline.getCustomTime() },
+
+              setCustomTime: function (time)
+              {
+                _timeline.setCustomTime(time);
+
+                this.customDate = _timeline.getCustomTime();
+              }
+            });
+
+          _timeline.on('rangechange', function (period) { scope.timeline.rangeChange(period) });
+          _timeline.on('rangechanged', function (period) { scope.timeline.rangeChanged(period) });
+          _timeline.on('select', function (selected) { scope.timeline.select(selected) });
+          _timeline.on('timechange', function (period) { scope.timeline.timeChange(period) });
+          _timeline.on('timechanged', function (period) { scope.timeline.timeChanged(period) });
         }
-      };
+      }
     }
-  ]);
+  ]).
+
+
+  directive(
+  'timeNav', [
+    function ()
+    {
+      return {
+        restrict: 'E',
+        replace: false,
+        scope: {
+          timeline: '='
+        },
+        controller: function ($scope)
+        {
+          var start = 0;
+
+          $scope.timeline.setScope = function (scope)
+          {
+            $scope.timeline.scope = {
+              day: false,
+              week: false,
+              month: false,
+              year: false,
+              custom: false
+            };
+
+            $scope.timeline.scope[scope] = true;
+
+            if (scope != 'custom')
+            {
+              $scope.timeline.setWindow(
+                vis.moment().startOf(scope),
+                vis.moment().endOf(scope)
+              );
+
+              $scope.timeline.setOptions(
+                {
+                  min: vis.moment().startOf(scope).valueOf(),
+                  start: vis.moment().startOf(scope).valueOf(),
+                  max: vis.moment().endOf(scope).valueOf(),
+                  end: vis.moment().endOf(scope).valueOf()
+                }
+              );
+            }
+            else
+            {
+              $scope.timeline.setOptions(
+                {
+                  min: null,
+                  max: null
+                }
+              );
+            }
+
+            start = 0;
+          };
+
+          var scope;
+
+          $scope.timeline.stepScope = function (direction)
+          {
+            start = start + direction;
+
+            angular.forEach(
+              $scope.timeline.scope, function (active, _scope)
+              {
+                if (active) scope = _scope;
+              });
+
+            $scope.timeline.setWindow(
+              vis.moment().add(scope, start).startOf(scope),
+              vis.moment().add(scope, start).endOf(scope)
+            );
+
+            $scope.timeline.setOptions(
+              {
+                min: vis.moment().add(scope, start).startOf(scope).valueOf(),
+                start: vis.moment().add(scope, start).startOf(scope).valueOf(),
+                max: vis.moment().add(scope, start).endOf(scope).valueOf(),
+                end: vis.moment().add(scope, start).endOf(scope).valueOf()
+              });
+          };
+
+          //          setTimeout(
+          //            function () { $scope.timeline.setScope('month') },
+          //            25
+          //          );
+        }
+      }
+    }
+  ]).
+
+
+  filter(
+  'parseRange', [
+    function ()
+    {
+      return function (period)
+      {
+        var range = {
+          apart: function (date)
+          {
+            return {
+              year: vis.moment(date).get('year'),
+              month: {
+                number: vis.moment(date).get('month'),
+                name: vis.moment(date).format('MMMM')
+              },
+              week: vis.moment(date).format('w'),
+              day: {
+                number: vis.moment(date).get('date'),
+                name: vis.moment(date).format('dddd')
+              },
+              hour: vis.moment(date).format('HH'),
+              minute: vis.moment(date).format('mm'),
+              second: vis.moment(date).format('ss'),
+              milli: vis.moment(date).get('milliseconds')
+            }
+          },
+
+          analyse: function (period)
+          {
+            var p = {
+              s: this.apart(period.start),
+              e: this.apart(period.end)
+            };
+
+            // TODO: Choose for a more sensible name
+            var info = {
+              first: '',
+              second: '',
+              third: ''
+            };
+
+            if (p.s.year == p.e.year)
+            {
+              info = {
+                first: p.s.day.name + ' ' + p.s.day.number + '-' + p.s.month.name + '  -  ' +
+                       p.e.day.name + ' ' + p.e.day.number + '-' + p.e.month.name,
+                second: p.s.year,
+                third: ''
+              };
+
+              if (p.s.month.number == p.e.month.number)
+              {
+                info = {
+                  first: p.s.day.name + ' ' + p.s.day.number + '  -  ' +
+                         p.e.day.name + ' ' + p.e.day.number,
+                  second: p.s.month.name + ' ' + p.s.year,
+                  third: 'Month number: ' + Number(p.s.month.number + 1)
+                };
+
+                if (p.s.week == p.e.week)
+                {
+                  info.third += ', Week number: ' + p.s.week;
+                }
+                else
+                {
+                  info.third += ', Week numbers: ' + p.s.week + ' - ' + p.e.week;
+                }
+
+                if (p.s.day.number == p.e.day.number)
+                {
+                  if (p.e.hour == 23 &&
+                      p.e.minute == 59 &&
+                      p.e.second == 59 &&
+                      p.e.milli == 999)
+                  {
+                    p.e.hour = 24;
+                    p.e.minute = '00';
+                    p.e.second = '00';
+                    p.e.milli = '00';
+                  }
+
+                  info = {
+                    first: p.s.hour + ':' + p.s.minute + '  -  ' +
+                           p.e.hour + ':' + p.e.minute,
+                    second: p.s.day.name + ' ' + p.s.day.number + ' ' + p.s.month.name + ' ' + p.s.year,
+                    third: 'Week number: ' + p.s.week
+                  };
+
+                  if (p.s.hour == p.e.hour)
+                  {
+                    info = {
+                      first: p.s.hour + ':' + p.s.minute + ':' + p.s.second + '  -  ' +
+                             p.e.hour + ':' + p.e.minute + ':' + p.e.second,
+                      second: p.s.day.name + ' ' + p.s.day.number + ' ' + p.s.month.name + ' ' + p.s.year,
+                      third: 'Week number: ' + p.s.week
+                    };
+
+                    if (p.s.minute == p.e.minute)
+                    {
+                      info = {
+                        first: p.s.hour + ':' + p.s.minute + ':' + p.s.second + '.' + p.s.milli + '  -  ' +
+                               p.e.hour + ':' + p.e.minute + ':' + p.e.second + '.' + p.e.milli,
+                        second: p.s.day.name + ' ' + p.s.day.number + ' ' + p.s.month.name + ' ' + p.s.year,
+                        third: 'Week number: ' + p.s.week
+                      };
+                    }
+                  }
+                }
+              }
+            }
+            else
+            {
+              info = {
+                first: p.s.day.name + ' ' + p.s.day.number + '-' + p.s.month.name + ', ' + p.s.year
+                         + '  -  ' +
+                       p.e.day.name + ' ' + p.e.day.number + '-' + p.e.month.name + ', ' + p.e.year,
+                second: '',
+                third: 'Years: ' + p.s.year + ' - ' + p.e.year
+              };
+            }
+
+            return info.first + ', ' + info.second + ', ' + info.third;
+          }
+        };
+
+        return range.analyse(period);
+      }
+    }
+  ]
+);
--- app/scripts/timelineCtrl.js
+++ app/scripts/timelineCtrl.js
@@ -4,8 +4,8 @@ angular.module('ngVisJsApp')
   .controller(
   'TimelineCtrl',
   [
-    '$rootScope', '$scope', '$location',
-    function ($rootScope, $scope, $location)
+    '$rootScope', '$scope', '$location', '$timeout',
+    function ($rootScope, $scope, $location, $timeout)
     {
       $rootScope.pageIndex = 1;
 
@@ -13,7 +13,7 @@ angular.module('ngVisJsApp')
         '01. Basic usage',
         '02. Interactive',
         '03. Lots of data',
-        '06. HTML data',
+        '04. HTML data',
         '05. Groups',
         '06. Event listeners',
         '07. Custom time bar',
@@ -26,8 +26,173 @@ angular.module('ngVisJsApp')
         '14. Item class names'
       ];
 
-      $scope.setExample = function (index)
+      $scope.getExample = function (index)
       {
+        var data,
+            options = {
+              defaults: true
+            };
+
+        var now = vis.moment().minutes(0).seconds(0).milliseconds(0);
+
+        switch (index)
+        {
+          case 1:
+            data = [
+              {id: 1, content: 'item 1', start: '2014-06-20'},
+              {id: 2, content: 'item 2', start: '2014-06-14'},
+              {id: 3, content: 'item 3', start: '2014-06-18'},
+              {id: 4, content: 'item 4', start: '2014-06-16', end: '2014-06-19'},
+              {id: 5, content: 'item 5', start: '2014-06-25'},
+              {id: 6, content: 'item 6', start: '2014-06-27', type: 'point'}
+            ];
+
+            $scope.info = {
+              range: ''
+            };
+
+            break;
+
+          case 2:
+            options = {
+              start: '2014-01-10',
+              end: '2014-02-10',
+              orientation: 'top',
+              height: '300px',
+              editable: true,
+              /* alternatively, enable/disable individual actions:
+               editable: {
+               add: true,
+               updateTime: true,
+               updateGroup: true,
+               remove: true
+               },
+               */
+              showCurrentTime: true
+            };
+
+            data = [
+              {id: 1, content: 'item 1<br>start', start: '2014-01-23'},
+              {id: 2, content: 'item 2', start: '2014-01-18'},
+              {id: 3, content: 'item 3', start: '2014-01-21'},
+              {id: 4, content: 'item 4', start: '2014-01-19', end: '2014-01-24'},
+              {id: 5, content: 'item 5', start: '2014-01-28', type: 'point'},
+              {id: 6, content: 'item 6', start: '2014-01-26'}
+            ];
+            break;
+
+          case 3:
+            options = {
+              editable: true,
+              zoomMin: 1000 * 60 * 60 * 24,
+              zoomMax: 1000 * 60 * 60 * 24 * 30 * 3
+            };
+
+            $scope.drawLotsOfDataValue = 10;
+
+            var produceSlots = function (desired)
+            {
+              var now = vis.moment().minutes(0).seconds(0).milliseconds(0);
+
+              var count = parseInt(desired) || 100;
+              var newData = [];
+
+              for (var i = 0; i < count; i ++)
+              {
+                newData.push(
+                  {
+                    id: i,
+                    content: 'item ' + i,
+                    start: now + (options.zoomMin * i)
+                  }
+                );
+              }
+
+              return newData;
+            };
+
+            data = produceSlots(10);
+
+            $scope.drawLotsOfData = function ()
+            {
+              // console.log('coming in here ->');
+              $scope.data = produceSlots($scope.drawLotsOfDataValue);
+//              $timeout(
+//                function ()
+//                {
+//
+//                }
+//              );
+            };
+
+            break;
+
+
+          case 4:
+            var item1 = document.createElement('div');
+            item1.appendChild(document.createTextNode('item 1'));
+
+            var item2 = document.createElement('div');
+            item2.innerHTML = '<span>item 2</span>';
+
+            var item3 = document.createElement('div');
+            var span3 = document.createElement('span');
+            span3.className = 'large';
+            span3.appendChild(document.createTextNode('item 3'));
+            item3.appendChild(span3);
+
+            var item4 = 'item <span class="large">4</span>';
+
+            var item5 = document.createElement('div');
+            item5.appendChild(document.createTextNode('item 5'));
+            item5.appendChild(document.createElement('br'));
+            var img5 = document.createElement('img');
+            img5.src = 'img/attachment-icon.png';
+            img5.style.width = '48px';
+            img5.style.height = '48px';
+            item5.appendChild(img5);
+
+            var item6 = 'item6<br><img src="img/comments-icon.png" style="width: 48px; height: 48px;">';
+
+            var item7 = 'item7<br><a href="http://visjs.org" target="_blank">click here</a>';
+
+            data = [
+              {id: 1, content: item1, start: '2013-06-20'},
+              {id: 2, content: item2, start: '2013-06-14'},
+              {id: 3, content: item3, start: '2013-06-18'},
+              {id: 4, content: item4, start: '2013-06-16', end: '2013-06-19'},
+              {id: 5, content: item5, start: '2013-06-25'},
+              {id: 6, content: item6, start: '2013-06-27'},
+              {id: 7, content: item7, start: '2013-06-21'}
+            ];
+            break;
+
+          case 5:
+            options = {
+              groupOrder: 'content'  // groupOrder can be a property name or a sorting function
+            };
+
+            data = {
+              John: [
+                {id: 1, content: 'item 1', start: '2014-06-18'},
+                {id: 2, content: 'item 2', start: '2014-06-18'}
+              ],
+              Alston: [
+                {id: 3, content: 'item 3', start: '2014-06-16'},
+                {id: 4, content: 'item 4', start: '2014-06-14', end: '2014-06-17'}
+              ],
+              Lee: [
+                {id: 5, content: 'item 5', start: '2014-06-23'},
+                {id: 6, content: 'item 6', start: '2014-06-25'}
+              ]
+            };
+            break;
+        }
+
+        return {
+          options: options,
+          data: data
+        }
       };
 
       $scope.setView = function (hash, example)
@@ -59,10 +224,217 @@ angular.module('ngVisJsApp')
           $scope.exampleIndex = id;
 
           $location.search({id: id});
+
+          var values = $scope.getExample(id);
+
+          $scope.options = values.options;
+          $scope.data = values.data;
         }
       };
 
       $scope.setView((($location.hash()) != '') ? $location.hash() : 'introduction');
+
+      // $scope.loadData('simple');
+
+      var debug = false;
+
+
+
+
+      $scope.simplifyItems = function (items)
+      {
+        var simplified = [];
+
+        angular.forEach(
+          items,
+          function (group, label)
+          {
+            angular.forEach(
+              group,
+              function (item)
+              {
+                item.group = label;
+
+                simplified.push(item);
+              }
+            );
+          }
+        );
+
+        return simplified;
+      };
+
+
+      /**
+       * Timeline stuff
+       */
+      $scope.timeline = {
+
+        select: function (selected)
+        {
+          if (debug)
+          {
+            console.log('selected items: ', selected.items);
+          }
+
+          var items = $scope.simplifyItems($scope.items);
+
+          var format = 'YYYY-MM-DDTHH:mm';
+
+          angular.forEach(
+            items,
+            function (item)
+            {
+              if (item.id == selected.items[0])
+              {
+                $scope.slot = {
+                  id: item.id,
+                  start: moment(item.start).format(format),
+                  end: (item.end) ? moment(item.end).format(format) : null,
+                  content: item.content
+                };
+
+                $scope.$apply();
+              }
+            }
+          );
+        },
+
+        range: {},
+
+        rangeChange: function (period)
+        {
+          this.range = $scope.timeline.getWindow();
+
+          if (! $scope.$$phase)
+          {
+            $scope.$apply();
+          }
+
+          if (debug)
+          {
+            console.log('rangeChange: start-> ', period.start, ' end-> ', period.end);
+          }
+        },
+
+        rangeChanged: function (period)
+        {
+          if (debug)
+          {
+            console.log('rangeChange(d): start-> ', period.start, ' end-> ', period.end);
+          }
+        },
+
+        customTime: null,
+
+        timeChange: function (period)
+        {
+          if (debug)
+          {
+            console.log('timeChange: ', period.time);
+          }
+
+          $scope.$apply(
+            function ()
+            {
+              $scope.timeline.customTime = period.time;
+            }
+          );
+        },
+
+        timeChanged: function (period)
+        {
+          if (debug)
+          {
+            console.log('timeChange(d): ', period.time);
+          }
+        },
+
+        slot: {
+          add: function (item, callback)
+          {
+            item.content = prompt('Enter text content for new item:', item.content);
+
+            if (item.content != null)
+            {
+              callback(item); // send back adjusted new item
+            }
+            else
+            {
+              callback(null); // cancel item creation
+            }
+          },
+
+          move: function (item, callback)
+          {
+            if (confirm(
+                'Do you really want to move the item to\n' +
+                'start: ' + item.start + '\n' +
+                'end: ' + item.end + '?'))
+            {
+              callback(item); // send back item as confirmation (can be changed
+            }
+            else
+            {
+              callback(null); // cancel editing item
+            }
+          },
+
+          update: function (item, callback)
+          {
+            item.content = prompt('Edit items text:', item.content);
+
+            if (item.content != null)
+            {
+              callback(item); // send back adjusted item
+            }
+            else
+            {
+              callback(null); // cancel updating the item
+            }
+          },
+
+          remove: function (item, callback)
+          {
+            if (confirm('Remove item ' + item.content + '?'))
+            {
+              callback(item); // confirm deletion
+            }
+            else
+            {
+              callback(null); // cancel deletion
+            }
+          }
+        }
+      };
+
+      //    $scope.getCustomTime = function () {
+      //      $scope.gotCustomDate = $scope.timeline.getCustomTime();
+      //    };
+      //
+      //    $scope.getSelection = function () {
+      //      $scope.gotSelection = $scope.timeline.getSelection();
+      //    };
+      //
+      //    $scope.setSelection = function (selection) {
+      //      selection = (angular.isArray(selection)) ? selection : [].concat(selection);
+      //
+      //      $scope.timeline.setSelection(selection);
+      //    };
+      //
+      //    $scope.getWindow = function () {
+      //      $scope.gotWindow = $scope.timeline.getWindow();
+      //    };
+      //
+      //    $scope.setWindow = function (start, end) {
+      //      $scope.timeline.setScope('custom');
+      //
+      //      $scope.timeline.setWindow(start, end);
+      //    };
+
+      //    $scope.setOptions = function (options) {
+      //      $scope.timeline.setOptions(options);
+      //    };
     }
   ]
 );
